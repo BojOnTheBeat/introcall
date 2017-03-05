ea313854bdaca466422fc0919cdf83caae38c1bf
Bolaji Fatade <bjfatade@gmail.com>
2017-03-04 19:01:54 -0500

    Register/LogIn navigation using accounts-password package

--- introcall/server/main.js
+++ introcall/server/main.js
@@ -2,4 +2,17 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+	Router.route('/', {
+		name: 'home',
+    	template: 'home'
+	});
+
+	Router.configure({
+    	layoutTemplate: 'main'
+	});
+
+	Router.route('/register');
+	Router.route('/login');
+
 });
