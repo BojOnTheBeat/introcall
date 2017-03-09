4e3cd1bd631c5894f83d9cf858885d9087a910c4
Isa Hassen <hassen.isa@gmail.com>
2017-03-09 08:48:01 -0500

    autostyling

--- introcall/client/main.js
+++ introcall/client/main.js
@@ -1,10 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {
+    Template
+} from 'meteor/templating';
+import {
+    ReactiveVar
+} from 'meteor/reactive-var';
 
 import './main.html';
 
 Router.route('/', {
-	name: 'home',
+    name: 'home',
     template: 'home'
 });
 
@@ -16,28 +20,28 @@ Router.route('/register');
 Router.route('/login');
 
 Template.register.events({
-    'submit form': function(e){
+    'submit form': function (e) {
         e.preventDefault();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
 
         //Meteor magic
         Accounts.createUser({
-    		email: email,
-    		password: password
-		}, function(error){
-    		if(error){
-        	console.log(error.reason); // Output error if registration fails
-    		} else {
-        		Router.go("home"); // Redirect user if registration succeeds
-    		}
-	});
+            email: email,
+            password: password
+        }, function (error) {
+            if (error) {
+                console.log(error.reason); // Output error if registration fails
+            } else {
+                Router.go("home"); // Redirect user if registration succeeds
+            }
+        });
 
     },
 });
 
 Template.navigation.events({
-    'click .logout': function(event){
+    'click .logout': function (event) {
         event.preventDefault();
         Meteor.logout();
         Router.go('login');
@@ -45,17 +49,17 @@ Template.navigation.events({
 });
 
 Template.login.events({
-    'submit form': function(event){
+    'submit form': function (event) {
         event.preventDefault();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
-        Meteor.loginWithPassword(email, password, function(error){
-    		if(error){
-        		console.log(error.reason);
-    		} else {
-        		Router.go("home");
-    }
-});
+        Meteor.loginWithPassword(email, password, function (error) {
+            if (error) {
+                console.log(error.reason);
+            } else {
+                Router.go("home");
+            }
+        });
     }
 });
 
@@ -77,4 +81,3 @@ Template.login.events({
 //     instance.counter.set(instance.counter.get() + 1);
 //   },
 // });
-
