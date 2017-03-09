d4d60618bd31d8b0086cbf8e8f028c9a61f94953
Isa Hassen <hassen.isa@gmail.com>
2017-03-09 10:32:16 -0500

    add ConsultantProfile schema template

--- introcall/server/main.js
+++ introcall/server/main.js
@@ -1,15 +1,41 @@
-import { Meteor } from 'meteor/meteor';
+import {
+	Meteor
+} from 'meteor/meteor';
+
+ConsultantProfiles = new Mongo.Collection('consultantprofiles');
+
+ConsultantProfiles.schema = new SimpleSchema({
+	userId: {
+		type: String,
+		regEx: SimpleSchema.RegEx.Id
+	},
+
+	avatarUrl: {
+		type: String,
+		regEx: SimpleSchema.RegEx.Url, //TODO: need to prevent CSRF attacks on this
+		label: "Avatar URL",
+		autoValue: function() { return 'http://placehold.it/120x120' }
+	},
+
+	tagline: {
+		type: String,
+		min: 3,
+		max: 140
+	}
+});
+
+ConsultantProfiles.attachSchema(ConsultantProfiles.schema);
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// code to run on server at startup
 
 	Router.route('/', {
 		name: 'home',
-    	template: 'home'
+		template: 'home'
 	});
 
 	Router.configure({
-    	layoutTemplate: 'main'
+		layoutTemplate: 'main'
 	});
 
 	Router.route('/register');
