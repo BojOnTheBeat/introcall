195db984cdedafed868ce1af3a01b1c2d296b5ea
Isa Hassen <hassen.isa@gmail.com>
2017-03-10 08:44:42 -0500

    rename to client.js and server.js

--- introcall/server/server.js
+++ introcall/server/server.js
@@ -29,16 +29,4 @@ ConsultantProfiles.attachSchema(ConsultantProfiles.schema);
 Meteor.startup(() => {
 	// code to run on server at startup
 
-	Router.route('/', {
-		name: 'home',
-		template: 'home'
-	});
-
-	Router.configure({
-		layoutTemplate: 'main'
-	});
-
-	Router.route('/register');
-	Router.route('/login');
-
 });
