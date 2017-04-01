561205dd4f861def9854132477ea09f00be3f5f2
Isa Hassen <hassen.isa@gmail.com>
2017-03-31 20:16:51 -0400

    im done with docs

--- introcall/client/templates/docs.js
+++ introcall/client/templates/docs.js
@@ -2,10 +2,10 @@ Template.docs.helpers({
     getComment : function(rid, method){
         docs = {
             '/publications/api-routes': {
-                'get': 'get a list of all available REST API routes'
+                'get': 'Get a list of all available REST API routes. See docs here: https://atmospherejs.com/simple/rest#listing-all-api-methods'
             },
             '/publications/meteor_autoupdate_clientVersions' : {
-                'get': ''
+                'get': 'Meteor.js internal framework method. Used to determine out-of-date client app versions. See Meteor docs.'
             },
             '/UserProfile':{
                 'post': 'Add a new User Profile/Simple sign up \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
@@ -13,14 +13,13 @@ Template.docs.helpers({
                 \n curl --request GET --url https://alpha.introcall.io//UserProfile'
             },
             '/UserProfile/:_id':{
-                'options':'',
+                'options':'hide',
                 'patch':'Update the User Profile with _id',
                 'delete': 'Delete the userProfile with _id'
             },
             '/Keywords' : {
-                'post': 'Add a new keyword n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile'
+                'post': 'Add a new keyword \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
+                \n \t \t - word: string  \n '
             },
             '/Keywords/:_id': {
                 'patch':'Update the keyword with _id',
@@ -29,9 +28,7 @@ Template.docs.helpers({
             },
             '/messages' : {
                 'options' : '',
-                'post' : 'Add a new message n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile'
+                'post' : 'Add a new message. See model.js for optional/required message params.'
             },
             '/messages/:_id' : {
                 'options':'',
@@ -39,44 +36,32 @@ Template.docs.helpers({
                 'delete': 'Delete the message with _id'
             },
             '/publications/users.public': {
-                'get' : 'Get a list of all users \n Response: 200 \n \t - content-type: application/json \n \t - body: list of User objects\
-                \n curl --request GET --url https://alpha.introcall.io//publications/users.public'
+                'get' : 'Get a list of all users (UserProfile objects) \n '
             },
             '/publications/keywords.public': {
-                'get' : 'Get a list of all keywords \n Response: 200 \n \t - content-type: application/json \n \t - body: Keyword object \n \t \t - _id: keyword id \n \t \t - word: (string) actual keyword string \n \n \
-                \n curl --request GET --url https://alpha.introcall.io//publications/users.public',
+                'get' : 'Get a list of all keywords',
             },
             '/publications/user.private': {
-                'get' : '????'
+                'get' : 'Get the current logged-in-user private fields.'
             },
             '/publications/messages': {
                 'get' : 'get all messages the logged in user has been involved in \n Response: 200 \n \t - content-type: application/json \n \t - body: list of message objects \
                 \n curl --request GET --url https://alpha.introcall.io/publications/messages?=  --header \'authorization: Basic YmpmYXRhZGVAZ21haWwuY29tOmludHJvY2FsbA==\''
             },
             '/methods/user.updateProfile': {
-                'post' : 'A logged in user can update their profile \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile'
+                'post' : 'Update the logged-in user profile '
             },
             '/methods/insertMessage' : {
-                'post' : 'Insert a new message \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile'
+                'post' : 'Send a new message for a given userid'
             },
             '/methods/connectTimekit' : {
-                'post' : 'Connect to TimeKit Api \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile'
+                'post' : 'Connect to TimeKit Api '
             },
             '/methods/timekit.calendarList' : {
-                'post' : 'Add a new Calendar ? \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile'
+                'post' : 'Update the list of Timekit calendar ids associated with a user account. '
             },
             '/methods/notify' : {
-                'post' : 'Notify user of a new message \n Request: 200 \n \t - content-type: application/json \n \t - body: user object\
-                \n \t \t - email: unique email address \n \t \t - password: unique password \n \
-                \n curl --request GET --url https://alpha.introcall.io//UserProfile',
+                'post' : 'Notify user of a new message ',
                 'options': ''
             },
 
